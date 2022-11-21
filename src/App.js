126bc0cc39231e894789c45443412a62a249b64a
Yuvraj Singh Sisodiya <63532070+yuvrxj-afk@users.noreply.github.com>
2022-11-21 07:06:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../src/components/NavBar";
+import Main from "../src/components/Main";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Navbar />
+
+      <Main />
+    </div>
+  );
+}
---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Main() {
+  return (
+    <main>
+      <h1 className="main-h">Fun facts about React</h1>
+      <ul className="list">
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on GitHub</li>
+        <li>Is maintained by Meta</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </main>
+  );
+}
